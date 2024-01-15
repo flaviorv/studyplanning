c827e1b22b35a666ab4e72594daacadb76fe13d4
flaviorv <frezendevaroto@gmail.com>
2024-01-15 02:14:03 -0300

    student class sketch

---
+++ src/test/java/com/frv/studyplanning/StudyplanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frv.studyplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyplanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
