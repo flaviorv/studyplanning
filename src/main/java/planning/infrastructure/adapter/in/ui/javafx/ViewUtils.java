c1df0c50000d45f7bb94d3a272160ff5d8ce319b
flaviorv <frezendevaroto@gmail.com>
2025-04-19 22:08:33 -0300

    Taking advantage of the last screen, avoiding resizing.

--- src/main/java/planning/infrastructure/adapter/in/ui/javafx/ViewUtils.java
+++ src/main/java/planning/infrastructure/adapter/in/ui/javafx/ViewUtils.java
@@ -2,18 +2,17 @@ package planning.infrastructure.adapter.in.ui.javafx;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
 public class ViewUtils {
     protected static void changeView(ActionEvent event, String view) throws IOException {
         Parent loader = FXMLLoader.load(Objects.requireNonNull(ViewUtils.class.getResource("/view/" + view + ".fxml")));
-        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-        stage.setScene(new Scene(loader));
-        stage.show();
+        Scene currentScene = ((Node) event.getSource()).getScene();
+        currentScene.setRoot(loader);
     }
 
 }
